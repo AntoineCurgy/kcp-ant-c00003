862181a9ac596d304bc95c167ef6cd46588de9af
AntoineCurgy <48860064+AntoineCurgy@users.noreply.github.com>
2026-06-02 15:29:40 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const KCP_CONFIG = {
+  client_id:    'C00003',
+  client_name:  'Antoine',
+  telegram_bot: 'https://t.me/C00004_Antoine_bot',
+  notion: {
+    tiroirs:  'https://www.notion.so/34de29de1f32802eab18fad96e4a4278?v=36ae29de1f32808fa628000c8d3246b8',
+    reunions: 'https://www.notion.so/292e29de1f3280ea8e0ce41fb72868a9?v=292e29de1f32800a8a18000cb0dc5fc0',
+    todo:     'https://www.notion.so/349e29de1f3280c5a548ec6be1bbbeee?v=34ae29de1f3280dd978a000c96689df9',
+  },
+  webhooks: {
+    chat:    'https://hook.eu2.make.com/8rh94l9v2lk87hr6g1jredxban13ftqu',
+    history: 'https://hook.eu2.make.com/26bhrahrq5xipqcem9hpq3jvdgrmd1si',
+    tiroirs: 'https://hook.eu2.make.com/4kskheb6ie4q6nupygterd84uj5kok2g',
+    prompt:  'https://hook.eu2.make.com/q86mcoelj49v49gcobam9tbixur6xvm5',
+  },
+};
